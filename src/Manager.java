803563093b99fa8096f475db586b13277eeabb81
rafaeatul-kabir <rafaeatul@gmail.com>
2023-11-18 19:22:46 -0500

    added the constructor overloadings in both parent and child

--- src/Manager.java
+++ src/Manager.java
@@ -3,13 +3,12 @@ public class Manager extends Employee {
     private int newClients;
 
     // Constructor for Manager class
-    public Manager(String name, int birthYear,  int traveledDays, int newClients, double rate) {
+    public Manager(String name, int birthYear, int newClients, int traveledDays, double rate) {
         super(name, birthYear, rate);
         this.traveledDays = traveledDays;
         this.newClients = newClients;
     }
-
-    public Manager(String name, int birthYear, double rate, String vehicle, int traveledDays, int newClients) {
+    public Manager(String name, int birthYear, int newClients, int traveledDays, double rate, String vehicle) {
         super(name, birthYear, rate, vehicle);
         this.traveledDays = traveledDays;
         this.newClients = newClients;
